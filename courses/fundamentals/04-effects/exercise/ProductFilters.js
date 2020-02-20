8a1ba0ace235952c2fd6811490c0b41a1c6f83c5
David Khourshid <davidkpiano@gmail.com>
2020-02-19 22:57:31 -0500

    Exercise 4

--- courses/fundamentals/04-effects/exercise/ProductFilters.js
+++ courses/fundamentals/04-effects/exercise/ProductFilters.js
@@ -2,9 +2,24 @@ import React, { useState, useEffect } from 'react'
 import ProductFilterList from 'YesterTech/ProductFilterList'
 import { getCategories } from './utils'
 
+// Example: https://codesandbox.io/s/optimistic-jackson-g4hgj
+
 function ProductFilters() {
   // useState
-  const categories = null
+  const [categories, setCategories] = useState(null)
+
+  useEffect(() => {
+    let canceled = false
+
+    getCategories().then(categories => {
+      if (canceled) return
+      setCategories(categories)
+    })
+
+    return () => {
+      canceled = true
+    }
+  }, [])
 
   if (!categories) return <div>Loading Filters...</div>
 
