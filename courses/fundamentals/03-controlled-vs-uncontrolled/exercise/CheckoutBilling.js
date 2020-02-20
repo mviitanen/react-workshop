e224a67165304087414c70c5e2bc740e8d7150c0
David Khourshid <davidkpiano@gmail.com>
2020-02-19 21:53:52 -0500

    Billing work

--- courses/fundamentals/03-controlled-vs-uncontrolled/exercise/CheckoutBilling.js
+++ courses/fundamentals/03-controlled-vs-uncontrolled/exercise/CheckoutBilling.js
@@ -5,12 +5,23 @@ import Heading from 'YesterTech/Heading'
 
 function CheckoutBilling({ onSubmit }) {
   const [sameAsBilling, setSameAsBilling] = useState(false)
+  const [billingName, setBillingName] = useState('')
+  const [billingAddress, setBillingAddress] = useState('')
+  const [shippingName, setShippingName] = useState('')
+  const [shippingAddress, setShippingAddress] = useState('')
 
   function handleSubmit(event) {
     event.preventDefault()
     // When the fields are stored in state above, this fields variable can just be
     // an object filled with the field states. We don't need `serializeForm` anymore
-    const fields = serializeForm(event.target, { hash: true })
+    // const fields = serializeForm(event.target, { hash: true })
+    const fields = {
+      billingName,
+      billingAddress,
+      shippingName: sameAsBilling ? billingName : shippingName,
+      shippingAddress: sameAsBilling ? billingAddress : shippingAddress,
+    }
+    console.log(fields)
     onSubmit(sameAsBilling, fields)
   }
 
@@ -26,11 +37,29 @@ function CheckoutBilling({ onSubmit }) {
         <hr />
         <div className="form-field">
           <label htmlFor="billing:name">Name</label>
-          <input id="billing:name" type="text" required name="billingName" autoComplete="off" />
+          <input
+            id="billing:name"
+            type="text"
+            required
+            name="billingName"
+            autoComplete="off"
+            onChange={event => {
+              setBillingName(event.target.value)
+            }}
+          />
         </div>
         <div className="form-field">
           <label htmlFor="billing:address">Address</label>
-          <input id="billing:address" type="text" required name="billingAddress" />
+          <input
+            id="billing:address"
+            type="text"
+            required
+            name="billingAddress"
+            defaultValue={billingAddress}
+            onChange={event => {
+              setBillingAddress(event.target.value)
+            }}
+          />
         </div>
 
         <Heading as="h2" size={3}>
@@ -48,7 +77,18 @@ function CheckoutBilling({ onSubmit }) {
 
         <div className="form-field">
           <label htmlFor="shipping:name">Name</label>
-          <input id="shipping:name" type="text" required name="shippingName" autoComplete="off" />
+          <input
+            id="shipping:name"
+            type="text"
+            required
+            name="shippingName"
+            autoComplete="off"
+            value={sameAsBilling ? billingName : shippingName}
+            onChange={event => {
+              setShippingName(event.target.value)
+            }}
+            disabled={sameAsBilling}
+          />
         </div>
         <div className="form-field">
           <label htmlFor="shipping:address">Address</label>
@@ -58,6 +98,11 @@ function CheckoutBilling({ onSubmit }) {
             required
             name="shippingAddress"
             autoComplete="off"
+            value={sameAsBilling ? billingAddress : shippingAddress}
+            onChange={event => {
+              setShippingAddress(event.target.value)
+            }}
+            disabled={sameAsBilling}
           />
         </div>
 
--- courses/fundamentals/03-controlled-vs-uncontrolled/lecture/Quantity.js
+++ courses/fundamentals/03-controlled-vs-uncontrolled/lecture/Quantity.js
@@ -2,22 +2,38 @@ import React, { useState } from 'react'
 import { FaMinusCircle, FaPlusCircle } from 'react-icons/fa'
 import 'YesterTech/Quantity.scss'
 
+function QuantityInput({ quantity, onChange }) {
+  return (
+    <div className="input-container">
+      <div>{quantity}</div>
+      <input
+        type="number"
+        aria-label="quantity"
+        value={quantity}
+        onChange={event => {
+          onChange(parseInt(event.target.value || 0))
+        }}
+      />
+    </div>
+  )
+}
+
 function Quantity() {
-  const quantity = 1
+  const [quantity, setQuantity] = useState(1)
+
+  console.log(quantity)
 
   return (
     <div className="quantity-picker">
       <div>
         <div>
-          <button type="button" className="icon-button">
+          <button type="button" className="icon-button" onClick={() => setQuantity(quantity - 1)}>
             <FaMinusCircle />
           </button>
         </div>
-        <div className="input-container">
-          <input type="text" aria-label="quantity" defaultValue={quantity} />
-        </div>
+        <QuantityInput quantity={quantity} onChange={setQuantity} />
         <div>
-          <button type="button" className="icon-button">
+          <button type="button" className="icon-button" onClick={() => setQuantity(quantity + 1)}>
             <FaPlusCircle />
           </button>
         </div>
