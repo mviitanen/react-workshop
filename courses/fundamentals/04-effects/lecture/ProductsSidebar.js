98559834d81b6b22eb8503507cc0533d0984b3ef
Brad Westfall <brad@azpixels.com>
2020-02-27 09:45:31 -0700

    progress on workshop

--- courses/fundamentals/04-effects/lecture/ProductsSidebar.js
+++ courses/fundamentals/04-effects/lecture/ProductsSidebar.js
@@ -2,11 +2,25 @@ import React, { useState, useEffect } from 'react'
 import ProductFilters from 'YesterTech/ProductFilters'
 
 function ProductsSidebar() {
-  return (
+  const query = '(min-width: 800px)'
+  const [isWide, setIsWide] = useState(window.matchMedia(query))
+
+  useEffect(() => {
+    const media = window.matchMedia(query)
+    const listener = () => {
+      setIsWide(media.matches)
+    }
+    media.addListener(listener)
+    return () => {
+      media.removeListener(listener)
+    }
+  }, [])
+
+  return isWide ? (
     <aside>
       <ProductFilters />
     </aside>
-  )
+  ) : null
 }
 
 export default ProductsSidebar
