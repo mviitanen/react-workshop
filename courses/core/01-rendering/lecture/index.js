23fd10c888f6f254b53f145c6b84ed4e249ebc8e
Brad Westfall <bradwestfall@brads-mbp.lan>
2020-09-22 14:37:57 -0700

    progress on ws

--- courses/core/01-rendering/lecture/index.js
+++ courses/core/01-rendering/lecture/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import { FaRegStar, FaStar } from 'react-icons/fa'
-// import { MdShoppingCart } from 'react-icons/md'
+import { FaPlus } from 'react-icons/fa'
+import { MdShoppingCart } from 'react-icons/md'
 import 'YesterTech/styles/global-styles.scss'
 import './styles.scss'
 
-// let's go
+function App() {
+  function addToCart() {
+    console.log('add to the shopping cart')
+  }
+
+  return (
+    <div>
+      <Button onClick={addToCart}>
+        <MdShoppingCart />
+        Add to cart
+      </Button>
+    </div>
+  )
+}
+
+function Button({ onClick, children }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
