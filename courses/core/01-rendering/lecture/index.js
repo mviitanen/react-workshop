721306201a91ee25e1fe1167cad9d77b8119a62f
Brad Westfall <brad@azpixels.com>
2020-10-05 11:42:25 -0700

    progress on day one

--- courses/core/01-rendering/lecture/index.js
+++ courses/core/01-rendering/lecture/index.js
@@ -1,7 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import { MdShoppingCart } from 'react-icons/md'
+import { MdShoppingCart } from 'react-icons/md'
 import 'YesterTech/styles/global-styles.scss'
 import './styles.scss'
 
-// Let's go
+function ActionButton({ onClick, children }) {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  )
+}
+
+function App() {
+  function onClick() {
+    console.log('logic for adding to the shopping cart')
+  }
+
+  return (
+    <div>
+      <ActionButton onClick={onClick}>
+        <MdShoppingCart />
+        <span></span>Add to cart
+      </ActionButton>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
